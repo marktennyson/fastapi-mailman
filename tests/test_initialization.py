2bae73f003cf07fa674a7933c314e0bc2244157e
Aniket Sarkar <aniketsarkar1998@gmail.com>
2021-10-04 08:42:40 +0530

    update

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -3,8 +3,9 @@ import typing as t
 
 if t.TYPE_CHECKING:
     from fastapi_mailman import Mail
+    from fastapi_mailman.config import ConnectionConfig
 
-def test_init_mail(mail:"Mail", config):
+def test_init_mail(mail:"Mail", config:"ConnectionConfig"):
     new_mail = mail.init_mail(config)
 
     assert mail.state.__dict__ ==  new_mail.__dict__
